fe87d08ac6a3e4e23c5aede9315a2b0560eaf311
Muskan Singh <muskansingh4275@gmail.com>
2026-08-29 06:58:35 +0000

    comparisons in js

---
+++ 01-basics/01-comparison.js
@@ -0,0 +1,34 @@
+// git add .
+// git commit -m "your message"
+// git push
+
+// console.log(2>1) ; //t
+// console.log(2<1); //f
+// console.log(2>=1); //t
+// console.log(2<=1); //f
+// console.log(2==1); //f
+// console.log(2!=1); //t
+
+// console.log("2">1)//t
+// console.log("2"<1)//f
+// console.log("02">1)//t
+
+// console.log(null>0) //f
+// console.log(null>=0) //t
+// console.log(null==0) //f
+
+
+// console.log(undefined>0)//f
+// console.log(undefined<0)//f
+// console.log(undefined==0)//f
+
+
+//###strictly greater######
+console.log(2===1) //false
+console.log(2!==1) //true
+
+//ab wo dataype v check krega
+console.log(2==="2") //false
+console.log(2!=="2") //true
+
+
