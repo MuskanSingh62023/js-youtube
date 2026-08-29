b9a27ba5c73ae8398feeab279e68608b3a73e90e
Muskan Singh <muskansingh4275@gmail.com>
2026-08-29 06:34:02 +0000

    updated javascript basics

--- 01-basics/01-conversionOperation.js
+++ 01-basics/01-conversionOperation.js
@@ -17,8 +17,46 @@ let valueInNumber = Number(score)
 
 let IsLoggedIn=1
 let booleanIsLoggedIn = Boolean(IsLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1->true
 //""-> false
 //anshu->true
+
+//###############Operations##############
+let value=3
+let negValue= -value
+// console.log(negValue)
+
+//operations on numbers
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1="hello"
+let str2='baby'
+let str3=(str1+str2)
+// console.log(str3) //hellobaby
+
+//  console.log('1'+2) //12
+//  console.log(1+"2") //12
+//  console.log('1'+"2")//12
+ 
+//  console.log('1'+2+2) //122
+//  console.log(1+2+'2') //32
+
+// console.log(true) //true
+// console.log(+true) //1
+// console.log(+"") //0
+
+
+//prefix and postfix increment
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
+++gameCounter
+console.log(gameCounter)
+ 
---
+++ 01-basics/01-datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat every code as js new version
+
+//alert(3+3)// we r using node js not browser
+
+console.log(typeof "anshuman")
+console.log(typeof null)
+console.log(typeof undefined)
