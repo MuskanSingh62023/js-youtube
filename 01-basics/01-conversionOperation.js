7833a80a5e1747dba566db1e7d3bc8fcc64ec32c
Muskan Singh <muskansingh4275@gmail.com>
2026-08-29 05:40:18 +0000

    conversion of datatypes

---
+++ 01-basics/01-conversionOperation.js
@@ -0,0 +1,24 @@
+let score ="33abc"
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//coversion string to number
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber) //number
+//console.log(valueInNumber)         //NaN
+
+//score value 
+//33 -> 33
+//"33abc" -> NaN
+//null->0
+//undefined-> NaN
+//true/false -> 0/1
+//'anshu'-> NaN
+
+let IsLoggedIn=1
+let booleanIsLoggedIn = Boolean(IsLoggedIn)
+console.log(booleanIsLoggedIn)
+
+//1->true
+//""-> false
+//anshu->true
