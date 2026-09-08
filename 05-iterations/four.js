9e04291a3f9a1e9c15c1798c7dd4d6182e4679cf
Muskan Singh <muskansingh4275@gmail.com>
2026-09-08 10:21:07 +0000

    Updated project

---
+++ 05-iterations/four.js
@@ -0,0 +1,49 @@
+//for in loop in objects
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    // console.log(key)
+//     js
+// cpp
+// rb
+// swift
+    // console.log(`${key} shortcut is for ${myObject[key]}`);
+//     js shortcut is for javascript
+// cpp shortcut is for C++
+// rb shortcut is for ruby
+// swift shortcut is for swift by apple
+}
+
+//for in loop in arrays
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(key);
+    // 0
+    // 1
+    // 2
+    // 3
+    // 4
+    console.log(programming[key]);
+//     js
+// rb
+// py
+// java
+// cpp
+
+}
+ // for in loop in map
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key); //map is no iterable
+// }
