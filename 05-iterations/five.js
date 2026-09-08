64f4d8ad96a7ab9cf53324f35fd5a7daae1982ef
Muskan Singh <muskansingh4275@gmail.com>
2026-09-08 10:52:10 +0000

    Updated project

---
+++ 05-iterations/five.js
@@ -0,0 +1,59 @@
+//forEach loop
+const coding = ["js", "ruby", "java", "python", "cpp"]
+// // forEach(callBack function)
+// coding.forEach( function (val){
+//     console.log(val);
+// } )
+// js
+// ruby
+// java
+// python
+// cpp
+
+//Arrow function 
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+// } )
+// js 0 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// ruby 1 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// java 2 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// python 3 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+// cpp 4 [ 'js', 'ruby', 'java', 'python', 'cpp' ]
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languageFileName: "js"
+    },
+    {
+        languageName: "java",
+        languageFileName: "java"
+    },
+    {
+        languageName: "python",
+        languageFileName: "py"
+    },
+]
+
+myCoding.forEach( (item) => {
+    
+    console.log(item);
+} )
+// { languageName: 'javascript', languageFileName: 'js' }
+// { languageName: 'java', languageFileName: 'java' }
+// { languageName: 'python', languageFileName: 'py' }
+
+console.log(item.languageName);
+// javascript
+// java
+// python
